cf4068fa60f066704e406a9fb13a462cdf1f7bad
Charles Desiderio <charlesdesiderio@MacBook-of-the-Dead.local>
2020-12-08 10:02:36 -0600

    Mongoose dependency

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+// DEPENDENCIES
 const express = require('express')
-
+const mongoose = require('mongoose')
 const app = express();
+
+// CONFIGURATION
+
 const PORT = 3000
+
 
 app.get('/', (req, res) => {
   res.send('Success')
