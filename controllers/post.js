424c67bbd179bfcd3e1af0a8c6d2c0cc2536d3a6
Charles Desiderio <charlesdesiderio@MacBook-of-the-Dead.local>
2020-12-08 20:50:32 -0600

    Added verification middleware for posts

--- controllers/post.js
+++ controllers/post.js
@@ -6,10 +6,51 @@ const jwt = require('jsonwebtoken')
 const Post = require('../models/post.js')
 const posts = express.Router()
 
-posts.get('/', (req, res) => {
+// MIDDLEWARE
+const verifyToken = (req, res, next) => {
+  const bearerHeader = req.headers['authorization']
+  if (typeof bearerHeader !== 'undefined') {
+    const bearerToken = bearerHeader.split(' ')[1]
+    req.token = bearerToken
+    next()
+  } else {
+    res.status(403).json({
+      error: 'Forbidden'
+    })
+  }
+}
+
+posts.get('/', verifyToken, (req, res) => {
   res.status(200).json({
     message: 'Success'
   })
 })
+
+
+
+// Post schema reference
+// postCreator: {
+//   type: String,
+//   required: true
+// },
+// postDate: {
+//   type: String,
+//   required: true
+// },
+// postedText: {
+//   type: String,
+//   required: true
+// },
+// postLanguage: {
+//   type: String,
+//   required: true
+// },
+// likedUsers: [String],
+// comments: [{
+//     userId: String,
+//     commentDate: String,
+//     commentText: String
+//   }]
+
 
 module.exports = posts
