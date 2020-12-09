774a691cb037a03b4aa9bef13f1c73c5fe92ae66
Charles Desiderio <charlesdesiderio@MacBook-of-the-Dead.local>
2020-12-08 21:10:18 -0600

    Created ability to create and view posts

--- controllers/post.js
+++ controllers/post.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken')
 
 // MODELS
 const Post = require('../models/post.js')
+const User = require('../models/user.js')
 const posts = express.Router()
 
 // MIDDLEWARE
@@ -12,6 +13,7 @@ const verifyToken = (req, res, next) => {
   if (typeof bearerHeader !== 'undefined') {
     const bearerToken = bearerHeader.split(' ')[1]
     req.token = bearerToken
+    req.user = jwt.verify(req.token, process.env.SECRET_TOKEN)
     next()
   } else {
     res.status(403).json({
@@ -21,11 +23,42 @@ const verifyToken = (req, res, next) => {
 }
 
 posts.get('/', verifyToken, (req, res) => {
-  res.status(200).json({
-    message: 'Success'
+  Post.find({}, (err, foundPosts) => {
+    if (err) {
+      res.status(400).json({
+        error: err
+      })
+    } else {
+      res.status(200).json({
+        posts: foundPosts
+      })
+    }
   })
 })
 
+posts.post('/', verifyToken, (req, res) => {
+  const newPost = {
+    postCreator: req.user.user.userId,
+    postDate: Date.now(),
+    postedText: req.body.text,
+    postLanguage: req.body.language,
+    likedUsers: [],
+    comments: []
+  }
+
+  Post.create(newPost, (err, createdPost) => {
+    if (err) {
+      res.status(400).json({
+        error: err
+      })
+    } else {
+      res.status(200).json({
+        message: 'Success!',
+        post: createdPost
+      })
+    }
+  })
+})
 
 
 // Post schema reference
