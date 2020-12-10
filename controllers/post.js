4d4599bd9fbbc832ee11d33b3328918f81a6897f
Charles Desiderio <charlesdesiderio@MacBook-of-the-Dead.local>
2020-12-10 08:13:10 -0600

    Added error handling for likes

--- controllers/post.js
+++ controllers/post.js
@@ -63,22 +63,31 @@ posts.post('/', verifyToken, (req, res) => {
 posts.put('/like', verifyToken, (req, res) => {
   
   Post.findById(req.body.id, (err, foundPost) => {
-    let likes = foundPost.likedUsers;
-    console.log(foundPost)
-    console.log(foundPost.likedUsers.includes(req.user.user.userId))
-    if (likes.includes(req.user.user.userId)) {
-      likes.splice(likes.indexOf(req.user.user.userId), 1)
+    if (err) {
+      res.status(400).json({
+        error: err
+      })
     } else {
-      likes.push(req.user.user.userId)
+      let likes = foundPost.likedUsers;
+      console.log(foundPost)
+      console.log(foundPost.likedUsers.includes(req.user.user.userId))
+      if (likes.includes(req.user.user.userId)) {
+        likes.splice(likes.indexOf(req.user.user.userId), 1)
+      } else {
+        likes.push(req.user.user.userId)
+      }
+      let updatedPost = foundPost
+      updatedPost.likedUsers = likes;
+      Post.findByIdAndUpdate(req.body.id, updatedPost, (err, foundPost) => {
+        if (err) {
+          res.status(400).json({
+            error: err
+          })
+        } else {
+          res.json({foundPost: foundPost})
+        }
+      })
     }
-    let updatedPost = foundPost
-    updatedPost.likedUsers = likes;
-    Post.findByIdAndUpdate(req.body.id, updatedPost, (err, foundPost) => {
-      
-      res.json({foundPost: foundPost})
-    })
-
-    console.log(foundPost)
   })
 })
 
