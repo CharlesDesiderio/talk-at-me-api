6542a0032044f812928fdf63940e4da31061f529
Charles Desiderio <charlesdesiderio@MacBook-of-the-Dead.local>
2020-12-21 11:02:19 -0600

    I dunno more effing cooorrrrsss

--- server.js
+++ server.js
@@ -23,6 +23,13 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //MIDDLEWARE
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }))
